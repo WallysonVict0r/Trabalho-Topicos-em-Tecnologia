2f21ad03ddf1ac80d7d53b70217934ee43b7714b
WallysonVict0r <wallyson.correa@edu.unipar.br>
2024-08-28 21:04:25 -0300

    Commit_Correção

--- src/main/java/br/unipar/trabalho_triaca/Swiitch.js
+++ src/main/java/br/unipar/trabalho_triaca/Swiitch.js
@@ -1,22 +1,32 @@
 let currentPage = 0;
 
 function loadSwitches(page) {
+    console.log(`Loading page ${page}`);
     fetch(`http://localhost:8080/api/switches?page=${page}&size=10`)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             const tableBody = document.querySelector('#switchesTable tbody');
             tableBody.innerHTML = '';
 
             data.content.forEach(switchData => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                            <td>${switchData.id}</td>
-                            <td>${switchData.name}</td>
-                            <td>${switchData.status}</td>
+                            <td>${switchData.registro}</td>
+                            <td>${switchData.data}</td>
+                            <td>${switchData.hora}</td>
+                            <td>${switchData.hostname}</td>
+                            <td>${switchData.ipAddress}</td>
+                            <td>${switchData.macAddress}</td>
+                            <td>${switchData.marca}</td>
+                            <td>${switchData.modelo}</td>
+                            <td>${switchData.numSerie}</td>
+                            <td>${switchData.versao}</td>
+                            <td>${switchData.imobilizado}</td>
                         `;
                 tableBody.appendChild(row);
             });
-
+            console.log('Updated table:', tableBody.innerHTML);
             document.getElementById('prevPage').disabled = data.first;
             document.getElementById('nextPage').disabled = data.last;
         })
@@ -35,4 +45,7 @@ document.getElementById('nextPage').addEventListener('click', () => {
     loadSwitches(currentPage);
 });
 
-loadSwitches(currentPage);
+// Chame a função loadSwitches quando a página for carregada
+window.onload = function() {
+    loadSwitches(currentPage);
+}
